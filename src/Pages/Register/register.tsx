1859aec8b14054c603af5015cc469d3b4bdd8568
jaisriramr <jaisriram@textmercato.com>
2023-10-31 15:30:49 +0530

    fixed extenstion error

--- src/Pages/Register/register.tsx
+++ src/Pages/Register/register.tsx
@@ -1,10 +1,20 @@
 import { Fragment } from "react";
 import "./register.css";
+import MusicHubLogo from "../../assets/musichub-logo.png";
 
 const Register = () => {
   return (
     <Fragment>
-      <div className="register-container">Register</div>
+      <div className="register-container">
+        <nav className="register-navbar">
+          <img
+            src={MusicHubLogo}
+            alt="MusicHub Logo"
+            className="register-nav-logo"
+          />
+        </nav>
+        <main className="register-main"></main>
+      </div>
     </Fragment>
   );
 };
